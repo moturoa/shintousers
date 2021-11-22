7f35b8396e9f19dcf33fce1c7fcb404f8ad03a5e
remkoduursma <remko@shintolabs.nl>
2021-11-22 12:05:23 +0100

    support config sections

--- R/users_db_connection.R
+++ R/users_db_connection.R
@@ -6,10 +6,17 @@ users_db_connection <- function(dbusername, config_file = "conf/config.yml"){
   
     conf <- config::get(dbusername, file = config_file)
     
+    # cfg. allow_default_fallback in shintobag::shinto_db_connection
+    if(is.null(conf)){
+      conf <- config::get(dbusername, config = "default", file = config_file)
+    }
+    
+    port <- ifelse(is.null(conf$dbport),5432,conf$dbport)
+    
     DBI::dbConnect(RPostgres::Postgres(), 
                    dbname = "users", 
                    host = conf$dbhost, 
-                   port = 5432, 
+                   port = PORT, 
                    user = conf$dbuser, 
                    password = conf$dbpassword)
   
