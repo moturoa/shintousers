2a38d69b576d608d46c62bad9eec274ff651c05a
Remko Duursma <remko@shintolabs.nl>
2022-04-12 10:24:35 +0200

    doc

--- R/get_appname_from_url.R
+++ R/get_appname_from_url.R
@@ -1,10 +1,10 @@
 #' Read path to appname on rsconnect
+#' @export
 get_appname_from_url <- function(session = getDefaultReactiveDomain()){
   
-
-  if(is.null(session))return(NA)
+  if(is.null(session))return(NA_character_)
   
   urlpath <- session$clientData$url_pathname
-  gsub("/","",urlpath)
+  gsub("/","_",urlpath)
   
 }
--- R/users_r6_object.R
+++ R/users_r6_object.R
@@ -7,10 +7,19 @@ shintoUser <- R6::R6Class(
   
   public = list(
     
+    #' @description The DB connection to `shintousers`
     con = NULL,
+    
+    #' @description The postgres schema in the DB
     schema = NULL,
+    
+    #' @description The DB user name
     dbusername = NULL,
+    
+    #' @description The application (shiny) user name
     userid = NULL,
+    
+    #' @appname The (rsconnect) application name
     appname = NULL,
     
     
@@ -42,12 +51,14 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Close the DB connection
     close = function(){
       
       dbDisconnect(self$con)
       
     },
     
+    #' @description Do a `dbGetQuery` on the `shintousers` DB (accepts glue)
     query = function(txt, glue = TRUE){
       
       if(glue)txt <- glue::glue(txt)
@@ -58,6 +69,7 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Do a `dbExecute` on the `shintousers` DB (accepts glue)
     execute_query = function(txt, glue = TRUE){
       
       if(glue)txt <- glue::glue(txt)
@@ -68,6 +80,7 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description `dbWriteTable(..., append = TRUE)`
     append_data = function(table, data){
       
       try(
@@ -79,6 +92,7 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Read a table (from the default schema)
     read_table = function(table, lazy = FALSE){
       
       out <- dplyr::tbl(self$con, in_schema(self$schema, table))
@@ -91,6 +105,7 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Get last login for this user for this application (reads `shintousers.logins`)
     get_last_login = function(userid = NULL, appname = NULL){
       
       if(is.null(userid))userid <- self$userid
@@ -103,13 +118,18 @@ shintoUser <- R6::R6Class(
        as.POSIXct(out$timestamp, tz = "UTC")
     },
     
+    #' @description Update the last login for this user / appname (in table `shintousers.logins`)
     set_last_login = function(now = as.character(Sys.time())){
       
       self$execute_query(glue("UPDATE {self$schema}.logins SET timestamp = '{now}' 
                                  WHERE userid = '{self$userid}' and appname = '{self$appname}'"))
       
     },
-    
+  
+    #' @description Reads last login, and sets the current time as the new 'last login'. 
+    #' @details If user has never logged in, writes a new line in `shintousers.logins`,
+    #' otherwise updates the last login. 
+    #' @returns Returns (invisibly) the last login information
     get_and_set_last_login = function(){
       
       user_log <- self$get_last_login()
@@ -130,10 +150,10 @@ shintoUser <- R6::R6Class(
       
       self$set_last_login()
       
-    return(user_log)
+    return(invisible(user_log))
     },
     
-    
+    #' @description Gets the role for the current user (admin or viewer, typically)
     get_role = function(userid = NULL, appname = NULL){
       
       if(is.null(userid))userid <- self$userid
@@ -149,21 +169,8 @@ shintoUser <- R6::R6Class(
       
     },
     
-    get_group = function(userid = NULL, appname = NULL){
-      
-      if(is.null(userid))userid <- self$userid
-      if(is.null(appname))appname <- self$appname
-      
-      out <- self$query(glue::glue("select groep from {self$schema}.roles where userid = '{userid}' and appname = '{appname}'"))
-      
-      if(nrow(out) == 0 || is.na(out$groep[1]) || out$groep == ""){
-        return(NULL)
-      }
-      
-      jsonlite::fromJSON(out$groep)
-      
-    },
-    
+    #' @description Sets the role for a user.
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     set_role = function(userid, appname, role){
       
       if(is.null(self$get_role(userid, appname))){
@@ -187,38 +194,64 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Get groups that the current user belongs to. See also `is_in_group`
+    get_group = function(userid = NULL, appname = NULL){
+      
+      if(is.null(userid))userid <- self$userid
+      if(is.null(appname))appname <- self$appname
+      
+      out <- self$query(glue::glue("select groep from {self$schema}.roles where userid = '{userid}' and appname = '{appname}'"))
+      
+      if(nrow(out) == 0 || is.na(out$groep[1]) || out$groep == ""){
+        return(NULL)
+      }
+      
+      jsonlite::fromJSON(out$groep)
+      
+    },
+    
+    #' @description Is the user in this group? `$is_in_group("superuser")` -> bool
+    is_in_group = function(group){
+      
+      group %in% self$get_group()
+      
+    },
+    
+    #' @description Set the group for this user.
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     set_group = function(userid, appname, group){
       
       group <- jsonlite::toJSON(group)
       
-      # 
-      # if(is.null(self$get_group(userid, appname))){
-      #   
-      #   self$append_data(
-      #     "roles",
-      #     tibble(
-      #       userid = userid,
-      #       appname = appname,
-      #       groep = group,
-      #       comment = ""
-      #     )
-      #   )
-      #   
-      # } else {
-      #   
+      if(is.null(self$get_group(userid, appname))){
+
+        self$append_data(
+          "roles",
+          tibble(
+            userid = userid,
+            appname = appname,
+            groep = group
+          )
+        )
+
+      } else {
+
         self$execute_query(glue("UPDATE {self$schema}.roles SET groep = '{group}' 
                                  WHERE userid = '{userid}' and appname = '{appname}'"))
         
-      #}
+      }
       
     },
     
+    #' @description Remove a role for a user
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     remove_role = function(userid, appname){
     
       self$execute_query(glue("delete from {self$schema}.roles where userid = '{userid}' and appname = '{appname}'"))
       
     },
     
+    #' @description Does the current user have a role? `$has_role("admin")` --> bool
     has_role = function(role){
       
       roles <- self$get_role()
@@ -226,6 +259,8 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Add an application to the list of applications
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     add_application = function(appname, roles, groups, comment = ""){
       
       data <- tibble(
@@ -243,6 +278,8 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Set application available roles
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     set_application_roles = function(appname, roles){
       
       role_json <- jsonlite::toJSON(roles)
@@ -250,6 +287,8 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Set groups available in an application
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     set_application_groups = function(appname, groups){
       
       group_json <- jsonlite::toJSON(groups)
@@ -257,7 +296,8 @@ shintoUser <- R6::R6Class(
       
     },
     
-    
+    #' @description Get available roles for an application
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     get_application_roles = function(appname){
       
       out <- self$query(glue("select roles from {self$schema}.applications where appname = '{appname}'"))$roles
@@ -270,6 +310,8 @@ shintoUser <- R6::R6Class(
       
     },
     
+    #' @description Add an application to the list of applications
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     get_application_groups = function(appname){
       
       out <- self$query(glue("select groups from {self$schema}.applications where appname = '{appname}'"))$groups
@@ -282,13 +324,16 @@ shintoUser <- R6::R6Class(
       
     },
     
-    
+    #' @description Get list of available applications
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     get_applications = function(){
       
       sort(self$query(glue("select appname from {self$schema}.applications"))$appname)
       
     },
     
+    #' @description Remove an application 
+    #' @details !! Do not use in shiny applications (except shintousers_app) !!
     remove_application = function(appname){
       
       self$execute_query(glue("delete from {self$schema}.applications where appname = '{appname}'"))
